b4af880de9619c86226f7f6201d2bcd586674f8f
Jason Aeschliman <j.aeschliman@gmail.com>
2019-01-08 22:45:02 -0500

    add a failing test

--- stack.lisp
+++ stack.lisp
@@ -29,3 +29,48 @@
 (print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 (print (test1))
 (print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+(define (call-with-tag tag body handler)
+    (set-stack-mark tag)
+    (let* ((snapshot (body)) ;; TODO: how to tell if it is a snapshot or normal value?
+           (resume (lambda (v) (resume-stack-snapshot snapshot v)))
+           (result (handler resume)))
+      result))
+
+
+(define (test2)
+    (print
+     (call-with-tag
+      'foo
+      (lambda ()
+        ;; (mapcar (lambda (v)
+        ;;           ;; would be nice if we could pass value along with 'foo
+        ;;           (if (eq v 3) (snapshot-to-stack-mark 'foo)
+        ;;               v))
+        ;;         '(1 2 3 4 5))
+        (+i 7 (snapshot-to-stack-mark 'foo))
+        )
+      (lambda (k) (k 6)))))
+
+(print "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+(print (test2))
+(print "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+
+;;; Bummer, this doesn't work yet.
+(define (test3)
+    (print
+     (call-with-tag
+      'foo
+      (lambda ()
+        (mapcar (lambda (v)
+                  ;; would be nice if we could pass value along with 'foo
+                  (if (eq v 3) (snapshot-to-stack-mark 'foo)
+                      v))
+                '(1 2 3 4 5))
+        ;; (+i 7 (snapshot-to-stack-mark 'foo))
+        )
+      (lambda (k) (k 6)))))
+
+(print "++++++++++++++++++++++++++++++++++++++++++++++++++")
+(print (test3))
+(print "++++++++++++++++++++++++++++++++++++++++++++++++++")

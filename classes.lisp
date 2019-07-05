9ba70b1ac9b90c205468cace8deb02a6d7c880b9
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-05 07:55:41 -0400

    simple slot system

--- classes.lisp
+++ classes.lisp
@@ -1,4 +1,19 @@
+(define (get-slot obj slot)
+    ((class-get-metadata (class-of obj) 'slot-reader) obj slot))
+(define (set-slot obj slot value)
+    ((class-get-metadata (class-of obj) 'slot-writer) obj slot value))
+
 (define Pair (create-class 'Pair 2))
+
+(class-set-metadata Pair 'slot-reader (lambda (obj slot)
+                                        (case slot
+                                          (x (instance-get-ivar obj 0))
+                                          (y (instance-get-ivar obj 1)))))
+
+(class-set-metadata Pair 'slot-writer (lambda (obj slot value)
+                                        (case slot
+                                          (x (instance-set-ivar obj 0 value))
+                                          (y (instance-set-ivar obj 1 value)))))
 (print Pair)
 
 (define my-pair (instantiate-class Pair))
@@ -9,5 +24,9 @@
 (instance-set-ivar my-pair 1 'b)
 (print (instance-get-ivar my-pair 0))
 (print (instance-get-ivar my-pair 1))
+(set-slot my-pair 'x 'A)
+(set-slot my-pair 'y 'B)
+(print (get-slot my-pair 'x))
+(print (get-slot my-pair 'y))
 
 'done

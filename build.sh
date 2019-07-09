eb738bfd08984b4bc3368c1e25883f54ac7ceaf7
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-09 09:19:32 -0400

    build amber

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ rm -rf build
 mkdir -p build
 g++ main.cpp -Werror -O0 -std=c++14 -I$INCLUDE -F$INCLUDE -framework Cocoa -framework SDL2 -framework SDL2_image -o build/run-file -v
 cd build
+ln run-file amber
 ln run-file soak
 ln run-file repl
 ln run-file events

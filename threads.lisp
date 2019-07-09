45baa7d8a839214af8a580712d2a97d23d839b22
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-08 20:07:51 -0400

    delete dead comment

--- threads.lisp
+++ threads.lisp
@@ -13,11 +13,6 @@
 (defmacro repeat-slowly (delay count & forms)
   `(repeat ,count ,@forms (sleep-ms ,delay)))
 
-;; this has to be run via the metacompiler,
-;; becuase the cpp compiler re-runs the interp loop for each form,
-;; and the 'preemption' doesn't work correctly that way.
-;; going to take a good bit of thought to figure out how to do this with
-;; with the events based system...
 (define (say-bye)
     (repeat-slowly 500 200 (print '(world)))
   (print 'the-end))

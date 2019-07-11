a9d728a5f0f907aa8a0c6559ed44cffd98ac24f2
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-11 10:12:17 -0400

    start of a snapshot test facility

---
+++ run-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function run_snapshot() {
+    local file_name="$1"
+    local snap_name="$1.snapshot"
+    diff $snap_name <(./build/amber $file_name) >/dev/null && echo " OK $file_name" || echo " FAIL $file_name"
+}
+
+find ./test/snapshots -iname '*.lisp'  | while read line; do
+   run_snapshot $line
+done

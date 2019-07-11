7803829bb95d028db5fac4a55c0436fc2c861629
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-10 23:03:50 -0400

    clean up blitwork.lisp

--- blitwork.lisp
+++ blitwork.lisp
@@ -1,47 +1,31 @@
-(set-symbol-value 'set set-symbol-value)
-(set 'ignore1 (lambda (_)))
+(define screen-width 750)
+(define screen-height 500)
 
-(set 'colors
-     '(0xffff0000
-       0xffffff00
-       0xff00ff00
-       0xff00ffff
-       0xff0000ff
-       0xffff00ff
-       0xffffffff
-       0xff000000))
+(define screen-size (make-point screen-width screen-height))
 
-(set 'selected-color (car colors))
-(set 'screen-size #f)
+(define cow (load-image "./res/cow.png"))
+(define magnifier (make-image 128 128))
+(define scale-factor (i->f (/i 128 32)))
 
-(set 'cow (load-image "./res/cow.png"))
-(set 'magnifier (make-image 128 128))
-(set 'scale-factor (i->f (/i 128 32)))
-
-(set 'magnify (lambda (at-point)
-                (let ((p (point- at-point 16@16)))
-                  (blit-from-screen
-                   magnifier
-                   0@0 p (point+ p 32@32)
-                   scale-factor 
-                   0.0)
-                  (blit-to-screen
-                   magnifier
-                   (point- screen-size 256@256)
-                   200 0))))
+(define (magnify at-point)
+    (let ((p (point- at-point 16@16)))
+      (blit-from-screen
+       magnifier
+       0@0 p (point+ p 32@32)
+       scale-factor 
+       0.0)
+      (blit-to-screen
+       magnifier
+       (point- screen-size 256@256)
+       200 0)))
 
 
-;;;;;;; register event handlers
+(define (onshow size)
+     (screen-fill-rect 0@0
+                       (make-point (image-width cow) (image-height cow))
+                       0xffffffff)
+     (blit-to-screen cow 0@0 100 0))
 
-(set 'onshow (lambda (w h)
-               (set 'screen-size (make-point w h))
-               (screen-fill-rect 0@0
-                                 (make-point (image-width cow) (image-height cow))
-                                 selected-color)
-               (blit-to-screen cow 0@0 100 0)))
+(define onmousemove magnify)
 
-(set 'onmousemove magnify)
-(set 'onmousedown ignore1)
-(set 'onmousedrag ignore1)
-(set 'onkey ignore1)
-(set 'onframe ignore1)
+(request-display screen-width screen-height)

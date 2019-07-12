6ba43abc95851a87f181966fef5894faf56c7ca2
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-12 07:30:49 -0400

    fail snapshots when nonzero exit code
    I am not a bash expert.

--- run-tests.sh
+++ run-tests.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
+return_value=0
+
+function update_return_value() {
+    if [ $1 -ne 0 ]; then
+        return $1
+    fi
+    return $return_value
+}
+
 function run_snapshot() {
     local file_name="$1"
     local snap_name="$1.snapshot"
-    diff $snap_name <(./build/amber $file_name 2>/dev/null) >/dev/null \
-        && echo " OK $file_name" || echo " FAIL $file_name"
+    local out_file=`mktemp`
+
+    ./build/amber $file_name 2>/dev/null > $out_file
+    local status=$?
+
+    if [ $status -eq 0 ]; then
+        diff $snap_name $out_file >/dev/null
+        status=$? 
+    fi
+
+    if [ $status -eq 0 ];
+    then
+        echo " OK $file_name"
+    else
+        echo " FAIL $file_name"
+    fi
+
+    rm $out_file
+    update_return_value $status
+    return_value=$?
+    return $return_value
 }
 
 find ./test/snapshots -iname '*.lisp'  | while read line; do
    run_snapshot $line
 done
+
+exit $?

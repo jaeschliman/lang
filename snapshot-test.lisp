53d50789040604f5a1d76cb08e72215b9ad9851b
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-14 07:20:38 -0400

    testing notes

--- snapshot-test.lisp
+++ snapshot-test.lisp
@@ -1,5 +1,11 @@
 (print '(hello world))
 (fork (sleep-ms 1000)
-      (print "so long!"))
-(save-snapshot "./0.amb")
+      (print "so long!")) ;; expect this
+
+(let ()
+  (save-snapshot "./0.amb")
+  (print '(resumed from here))) ;; expect this
+
+;; this won't print in the new image b/c it has not been read into the thread.
+;; it will work once we are using the meat runner 
 (print '(hello again world!))

ee07c6ecb363d240b1ea16ca9f2bc88757aa05d6
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-14 07:20:56 -0400

    spelling

--- snapshot-test.lisp
+++ snapshot-test.lisp
@@ -7,5 +7,5 @@
   (print '(resumed from here))) ;; expect this
 
 ;; this won't print in the new image b/c it has not been read into the thread.
-;; it will work once we are using the meat runner 
+;; it will work once we are using the meta runner 
 (print '(hello again world!))

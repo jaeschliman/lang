b724514e6de2f0ced47cee450c0d12f3427897a2
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-16 07:05:07 -0400

    define some generic numeric comparison ops (= <= etc)

--- boot/generic-functions.lisp
+++ boot/generic-functions.lisp
@@ -73,27 +73,90 @@
 
 (define-arithmetic-op *
   ((Fixnum Fixnum) *i)
-  ((Float Float) *f)
-  ((Fixnum Float) (lambda (a b) (*f (i->f a) b)))
-  ((Float Fixnum) (lambda (a b) (*f a (i->f b)))))
+  ((Float Float)   *f)
+  ((Fixnum Float)  (lambda (a b) (*f (i->f a) b)))
+  ((Float Fixnum)  (lambda (a b) (*f a (i->f b)))))
 
 (define-arithmetic-op +
   ((Fixnum Fixnum) +i)
-  ((Float Float) +f)
-  ((Fixnum Float) (lambda (a b) (+f (i->f a) b)))
-  ((Float Fixnum) (lambda (a b) (+f a (i->f b)))))
+  ((Float Float)   +f)
+  ((Fixnum Float)  (lambda (a b) (+f (i->f a) b)))
+  ((Float Fixnum)  (lambda (a b) (+f a (i->f b)))))
 
 (define-arithmetic-op -
   ((Fixnum Fixnum) -i)
-  ((Float Float) -f)
-  ((Fixnum Float) (lambda (a b) (-f (i->f a) b)))
-  ((Float Fixnum) (lambda (a b) (-f a (i->f b)))))
+  ((Float Float)   -f)
+  ((Fixnum Float)  (lambda (a b) (-f (i->f a) b)))
+  ((Float Fixnum)  (lambda (a b) (-f a (i->f b)))))
 
 (define-arithmetic-op /
   ((Fixnum Fixnum) /i)
-  ((Float Float) /f)
-  ((Fixnum Float) (lambda (a b) (/f (i->f a) b)))
-  ((Float Fixnum) (lambda (a b) (/f a (i->f b)))))
+  ((Float Float)   /f)
+  ((Fixnum Float)  (lambda (a b) (/f (i->f a) b)))
+  ((Float Fixnum)  (lambda (a b) (/f a (i->f b)))))
+
+(define ComparisonOp (create-class 'ComparisonOp 1))
+
+(define (pairwise-compare compare items)
+    (let ((loop #f))
+      (set! loop (lambda (cmp lst)
+                   (if (nil? (cdr lst)) #t
+                       (and (cmp (car lst) (cadr lst))
+                            (loop cmp (cdr lst))))))
+      (loop compare items)))
+
+(define comparison-op-invoke
+    (lambda args
+      (let ((compare (instance-get-ivar (car args) 0)))
+        (pairwise-compare compare (cdr args)))))
+
+(class-set-applicator ComparisonOp comparison-op-invoke)
+
+(defmacro define-comparison-op (name & clauses)
+  (let ((compare (gensym))
+        (it (gensym)))
+    `(let ((,it (instantiate-class ComparisonOp))
+           (,compare (make-generic-function 2)))
+       (instance-set-ivar ,it 0 ,compare)
+       ,@(mapcar
+          (lambda (clause)
+            `(generic-function-add-method ,compare (list ,@(first clause))
+                                          ,(second clause)))
+          clauses)
+       (define ,name ,it))))
+
+(define-comparison-op <
+  ((Fixnum Fixnum) <i)
+  ((Float Float)   <f)
+  ((Fixnum Float)  (lambda (a b) (<f (i->f a) b)))
+  ((Float Fixnum)  (lambda (a b) (<f a (i->f b)))))
+
+(define-comparison-op >
+  ((Fixnum Fixnum) >i)
+  ((Float Float)   >f)
+  ((Fixnum Float)  (lambda (a b) (>f (i->f a) b)))
+  ((Float Fixnum)  (lambda (a b) (>f a (i->f b)))))
+
+(define-comparison-op =
+  ((Fixnum Fixnum) eq)
+  ((Float Float)   eq)
+  ((Fixnum Float)  (lambda (a b) (eq (i->f a) b)))
+  ((Float Fixnum)  (lambda (a b) (eq a (i->f b)))))
+
+(define (%<= a b) (or (< a b) (= a b)))
+(define (%>= a b) (or (> a b) (= a b)))
+
+(define-comparison-op <=
+  ((Fixnum Fixnum) %<=)
+  ((Float Float)   %<=)
+  ((Fixnum Float)  %<=)
+  ((Float Fixnum)  %<=))
+
+(define-comparison-op >=
+  ((Fixnum Fixnum) %>=)
+  ((Float Float)   %>=)
+  ((Fixnum Float)  %>=)
+  ((Float Fixnum)  %>=))
 
 (define length (make-generic-function 1))
 (generic-function-add-method length (list String) string-length)

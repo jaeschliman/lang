af82895d283c855ec6c47c066d20e4a1fd181329
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-16 05:31:24 -0400

    start cleaning up GF defs

--- boot/generic-functions.lisp
+++ boot/generic-functions.lisp
@@ -1,27 +1,37 @@
 (define GenericFunction1 (create-class 'GenericFunction1 1))
 
+(define (gf-lookup-1 ht class)
+  (let ((r (ht-at ht class)))
+    (if (nil? r) (ht-at ht #t)
+        r)))
+
+(define gf-store #f)
+(define (gf-store ht classes method)
+    (if (nil? (cdr classes))
+        (ht-at-put ht (car classes) method)
+        (let ((nxt (ht-at ht (car classes))))
+          (when (nil? nxt)
+            (ht-at-put ht (car classes) (make-ht))
+            (set! nxt (ht-at ht (car classes))))
+          (gf-store nxt (cdr classes) method))))
+
 (define (make-generic-function1)
     (let ((result (instantiate-class GenericFunction1)))
       (instance-set-ivar result 0 (make-ht))
       result))
 
-(define (generic-function-1-add-method fn class method)
+(define (generic-function-add-method fn classlist method)
+    ;; TODO: check classlist against arity
     (let ((ht (instance-get-ivar fn 0)))
-      (ht-at-put ht class method))
-  '())
+      (gf-store ht classlist method)
+      '()))
 
 (define (generic-function-1-invoke fn arg)
     (let* ((ht (instance-get-ivar fn 0))
-           (method (ht-at ht (class-of arg))))
+           (method (gf-lookup-1 ht (class-of arg))))
       (method arg)))
 
 (class-set-applicator GenericFunction1 generic-function-1-invoke)
-
-(define length (make-generic-function1))
-
-(generic-function-1-add-method length String string-length)
-(generic-function-1-add-method length Cons list-length)
-(generic-function-1-add-method length Null (lambda (_) 0))
 
 (define GenericFunction2 (create-class 'GenericFunction2 1))
 
@@ -29,15 +39,6 @@
     (let ((result (instantiate-class GenericFunction2)))
       (instance-set-ivar result 0 (make-ht))
       result))
-
-(define (generic-function-2-add-method fn class-list method)
-    (let ((ht (instance-get-ivar fn 0))
-          (class-a (first class-list)))
-      (when (nil? (ht-at ht class-a))
-        (ht-at-put ht class-a (make-ht)))
-      (set! ht (ht-at ht class-a))
-      (ht-at-put ht (second class-list) method))
-  '())
 
 (define (generic-function-2-invoke fn arg1 arg2)
     (let* ((ht (instance-get-ivar fn 0))
@@ -64,8 +65,8 @@
        (instance-set-ivar ,it 0 ,combine)
        ,@(mapcar
           (lambda (clause)
-            `(generic-function-2-add-method ,combine (list ,@(first clause))
-                                            ,(second clause)))
+            `(generic-function-add-method ,combine (list ,@(first clause))
+                                          ,(second clause)))
           clauses)
        (define ,name ,it))))
 
@@ -92,5 +93,10 @@
   ((Float Float) /f)
   ((Fixnum Float) (lambda (a b) (/f (i->f a) b)))
   ((Float Fixnum) (lambda (a b) (/f a (i->f b)))))
+
+(define length (make-generic-function1))
+(generic-function-add-method length (list String) string-length)
+(generic-function-add-method length (list Cons)   list-length)
+(generic-function-add-method length (list Null)   (lambda (_) 0))
 
 'done

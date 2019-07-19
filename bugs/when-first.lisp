a6fa05aa71561266de9594675648bd8360910fb4
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-19 05:47:22 -0400

    clean up bug

--- bugs/when-first.lisp
+++ bugs/when-first.lisp
@@ -1,17 +1,15 @@
 ;; change the first `when` to an `if` and it goes away
 
-(define (print-fl fl stream)
-    (when (<f fl 0.0) (stream-write-string stream "-"))
-  (let ((f (absf fl)))
-    (%print-integer (f->i f) stream 10)
-    (stream-write-char stream #\.)
-    (let* ((precision 5)
-           (rem (f->i (*f (powf 10.0 (i->f precision)) (remf f))))
-           (len (integer-digit-length rem 10))
-           (leading-zeroes (-i precision len)))
-      (dotimes (_ leading-zeroes) (stream-write-char stream #\0))
-      (%print-integer rem stream 10))))
+(define (bug a b)
+    (let () (print b))
+  (let ((a '()))
+    (print b)
+    (let* ((a '())
+           (a '())
+           (a '()))
+      (dotimes (_ 3) b)
+      (print b))))
 
-(print-fl -3.0 *standard-output*)
+(bug a b)
 
 'done

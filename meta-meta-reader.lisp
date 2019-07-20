d73eff988544c8fc49caece9597f375636e71d26
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-20 15:34:43 -0400

    fixes for meta reader

--- meta-meta-reader.lisp
+++ meta-meta-reader.lisp
@@ -5,7 +5,7 @@ meta meta {
   string-lit = lisp.string:x -> (cons 'seq (string-to-charlist x))
 
   br-body-char  = ~[\\\]] any
-  br-esc-char   = "\\" x:any -> (bracket-escaped-char-character x)
+  br-esc-char   = "\\" any:x -> (bracket-escaped-char-character x)
   br-char       = br-body-char | br-esc-char
   br-char-range = br-char:a "-" br-char:b -> (make-bracket-char-range a b)
   bracket-lit   = "[" (br-char-range | br-char)+:chs "]" -> (cons 'or chs)
@@ -15,20 +15,24 @@ meta meta {
   grouped = "(" rule-body:b ")" -> b
 
   lit     = extern | sym | string-lit | bracket-lit | grouped
-  lit-mod = meta-lit:lit mod:mod -> (list mod lit)
+  lit-mod = lit:lit mod:mod -> (list mod lit)
 
   atom = "~"?:negated (lit-mod | lit):item -> (if (nil? negated) item `(not ,item))
 
   pred   = "?" ws lisp.expr:it -> `(where ,it)
-  app    = (atom:rule ws ":" ident:as -> `(set! ,as ,rule)) | atom | pred
+  bind   = atom:rule ws ":" ident:as -> `(set! ,as ,rule)
+  app    = bind | atom | pred
   result = "->" ws lisp.expr:it -> `(return ,it)
 
-  rule-branch         = app+:as " "* result?:r -> (cons 'seq (append as (if (nil? r) '() (list r))))
+  rule-app            = " "* app
+  rule-branch         = rule-app+:as " "* result?:r -> (cons 'seq (append as (if (nil? r) '() (list r))))
   rule-body-list      = rule-branch:fst ws more-rule-body-list?:rst -> (cons fst rst)
   more-rule-body-list = "|" rule-body-list:rs -> rs
   rule-body           = rule-body-list:rs -> (cons 'or rs)
 
-  rule = ws sym:name ws "=" rule-body:b -> `(define-rule ,name ,b)
+  rule = ws sym:name ws "=" ws rule-body:b -> `(define-rule ,name ,b)
 
   block = ws "meta" ws sym:n ws "{" ws rule+:rs ws "}" -> (make-meta-definition n rs)
+
+  main = block | lisp.expr
 }

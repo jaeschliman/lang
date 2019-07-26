887511c2194dd28e64a776e31c514e88eb34e3ca
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-26 16:29:55 -0400

    more exports

--- boot/exports.lisp
+++ boot/exports.lisp
@@ -44,12 +44,14 @@
  onmousemove onmousedown onmousedrag onkey onshow request-display)
 
 (export
- load-image image-width image-height blit-to-screen screen-fill-rect)
+ make-image load-image image-width image-height blit-to-screen screen-fill-rect fill-rect)
 
 (export set-stack-mark snapshot-to-stack-mark resume-stack-snapshot)
 
 (export continuation? continuation-value)
 
 (export save-snapshot save-snapshot-and-exit)
+
+(export kill-thread current-thread)
 
 'done

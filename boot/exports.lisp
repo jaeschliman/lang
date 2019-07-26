76e52c740c5cc3636ee161fa59f6dccf4bdbea2c
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-26 15:35:55 -0400

    more exports

--- boot/exports.lisp
+++ boot/exports.lisp
@@ -4,17 +4,17 @@
 
 (export apply let lambda let* define set-symbol-value binding set set! *package* defparameter symbol-package)
 
-(export defmacro gensym intern)
+(export defmacro gensym intern & &opt)
 
 (export
- eq if not when unless cond case)
+ eq if not when unless cond case and or)
 
 (export
- list cons car cdr nil? nth
+ list cons append nil? nth
  first second third)
 
 (export
- caar cadr cdar cddr caaar caddr cdaar cdddr caaar caddr cdaar cdddr)
+ car cdr caar cadr cdar cddr caaar caddr cdaar cdddr caaar caddr cdaar cdddr)
 
 (export
  make-ht make-st ht-at ht-at-put)
@@ -29,7 +29,7 @@
  = <= >= * / % + - i->f f->i)
 
 (export
- +i -i +f -f /i /f %i %f *i *f >i >f)
+ +i -i +f -f /i /f %i %f *i *f >i >f <i <f)
 
 (export
  sin cos tan floor ceil rem pow log abs)
@@ -49,5 +49,7 @@
 (export set-stack-mark snapshot-to-stack-mark resume-stack-snapshot)
 
 (export continuation? continuation-value)
+
+(export save-snapshot save-snapshot-and-exit)
 
 'done

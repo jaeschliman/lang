3c3c0526ab245e71db4beb024ce7b189376e728f
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-26 22:03:38 -0400

    package fixes for tl-exception snapshots

--- test/snapshots/tl-exception-1.lisp
+++ test/snapshots/tl-exception-1.lisp
@@ -1,8 +1,10 @@
-(fork
- (sleep-ms 5)
- (print 'background-0)
- (sleep-ms 5)
- (print 'background-1))
+(let ((pkg *package*))
+  (fork
+   (binding ((*package* pkg))
+     (sleep-ms 5)
+     (print 'background-0)
+     (sleep-ms 5)
+     (print 'background-1))))
 
 (print `(before error))
 (+f 1.0 2)
--- test/snapshots/tl-exception-2.lisp
+++ test/snapshots/tl-exception-2.lisp
@@ -1,8 +1,10 @@
-(fork
- (sleep-ms 5)
- (print 'background-0)
- (sleep-ms 5)
- (print 'background-1))
+(let ((pkg *package*))
+  (fork
+   (binding ((*package* pkg))
+     (sleep-ms 5)
+     (print 'background-0)
+     (sleep-ms 5)
+     (print 'background-1))))
 
 (print `(before error))
 (kill-thread (current-thread))

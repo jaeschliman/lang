0178ef33252507ab97a9a9d62a44869e3441034c
Jason Aeschliman <j.aeschliman@gmail.com>
2019-07-28 20:41:33 -0400

    get tl-exception tests passing again

--- test/snapshots/tl-exception-0.lisp
+++ test/snapshots/tl-exception-0.lisp
@@ -1,11 +1,17 @@
+(define print-lock (make-semaphore #t))
+(define (p thing)
+    (semaphore-wait print-lock)
+  (print thing)
+  (signal-semaphore print-lock))
+
 (let ((pkg *package*))
   (fork
    (binding ((*package* pkg))
-     (sleep-ms 5)
-     (print 'background-0)
-     (sleep-ms 5)
-     (print 'background-1))))
+     (sleep-ms 15)
+     (p 'background-0)
+     (sleep-ms 15)
+     (p 'background-1))))
 
-(print `(before error))
+(p `(before error))
 (throw 'goodbye!)
-(print `(should not print))
+(p `(should not print))

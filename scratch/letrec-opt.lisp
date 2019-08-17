744ae90fab2b6a1ef823dfbb62f04b6ac065f46b
Jason Aeschliman <j.aeschliman@gmail.com>
2019-08-17 11:45:25 -0400

    new bug for letrec opts

--- scratch/letrec-opt.lisp
+++ scratch/letrec-opt.lisp
@@ -18,3 +18,19 @@
              (print (%stack-depth-in-bytes)))))
 
 
+(define (mappend f as)
+    (let loop ((as as) (f f) (acc '()))
+         (if (nil? as) (reverse-list acc)
+             (let ((r acc))
+               (dolist (b (reverse-list (f (car as))))
+                 (set! r (cons b r)))
+               (loop (cdr as) f r)))))
+
+(define (list-to-pairs l)
+    (let ((final (first l)))
+      (let loop ((as l) (bs (cdr l)) (acc '()))
+           (if (nil? bs) (reverse-list (cons (list (car as) final) acc))
+               (loop (cdr as) (cdr bs) (cons (list (car as) (car bs)) acc))))))
+
+(print (list-to-pairs '(a b c)))
+(print (list-to-pairs '(a b c d)))

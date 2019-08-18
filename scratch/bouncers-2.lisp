6986c44d68bbd00ca051fab0d78d44625db5a467
Jason Aeschliman <j.aeschliman@gmail.com>
2019-08-18 19:13:04 -0400

    bouncers-2: clean up a bit

--- scratch/bouncers-2.lisp
+++ scratch/bouncers-2.lisp
@@ -1,6 +1,6 @@
-;; (set '#/lang/*note-closed-over-vars* #t)
-;; (set '#/lang/*enable-inline-let-bound-lambdas* #t)
-;; (set '#/lang/*enable-inline-letrec-bound-lambdas* #t)
+(set '#/lang/*note-closed-over-vars* #t)
+(set '#/lang/*enable-inline-let-bound-lambdas* #t)
+(set '#/lang/*enable-inline-letrec-bound-lambdas* #t)
 ;; (set '#/lang/*trace-eval* #t)
 
 (defmacro sync (sem & body)
@@ -19,67 +19,67 @@
 (define back-buffer (make-image screen-width screen-height))
 
 (define (ordered? a b c)
-    (and (<i a b) (<i b c)))
+  (and (<i a b) (<i b c)))
 
 (define rainbow-colors
-    '(0xffff0000
-      0xffff8800
-      0xffffaa00
-      0xffffdd00
-      0xffffff00
-      0xff88ff00
-      0xffaaff00
-      0xffddff00
-      0xff00ff00
-      0xff00ff88
-      0xff00ffaa
-      0xff00ffdd
-      0xff00ffff
-      0xff0088ff
-      0xff00aaff
-      0xff00ddff
-      0xff0000ff
-      0xff8800ff
-      0xffaa00ff
-      0xffdd00ff
-      0xffff00ff
-      0xffff88ff
-      0xffffaaff
-      0xffffddff
-      0xffffffff
-      0xff888888
-      0xffaaaaaa
-      0xffdddddd
-      0xff000000
-      0xff880000
-      0xffaa0000
-      0xffdd0000))
+  '(0xffff0000
+    0xffff8800
+    0xffffaa00
+    0xffffdd00
+    0xffffff00
+    0xff88ff00
+    0xffaaff00
+    0xffddff00
+    0xff00ff00
+    0xff00ff88
+    0xff00ffaa
+    0xff00ffdd
+    0xff00ffff
+    0xff0088ff
+    0xff00aaff
+    0xff00ddff
+    0xff0000ff
+    0xff8800ff
+    0xffaa00ff
+    0xffdd00ff
+    0xffff00ff
+    0xffff88ff
+    0xffffaaff
+    0xffffddff
+    0xffffffff
+    0xff888888
+    0xffaaaaaa
+    0xffdddddd
+    0xff000000
+    0xff880000
+    0xffaa0000
+    0xffdd0000))
 
 (define (mappend f as)
-    (let loop ((as as) (f f) (acc '()))
-         (if (nil? as) (reverse-list acc)
-             (let ((r acc))
-               (dolist (b (reverse-list (f (car as))))
-                 (set! r (cons b r)))
-               (loop (cdr as) f r)))))
+  (let loop ((as as) (f f) (acc '()))
+       (if (nil? as) (reverse-list acc)
+           (let ((r acc))
+             (dolist (b (reverse-list (f (car as))))
+               (set! r (cons b r)))
+             (loop (cdr as) f r)))))
 
 (define (list-to-pairs l)
-    (let ((final (first l)))
-      (let loop ((as l) (bs (cdr l)) (acc '()))
-           (if (nil? bs) (reverse-list (cons (list (car as) final) acc))
-               (loop (cdr as) (cdr bs) (cons (list (car as) (car bs)) acc))))))
+  (let ((final (first l)))
+    (let loop ((as l) (bs (cdr l)) (acc '()))
+         (if (nil? bs) (reverse-list (cons (list (car as) final) acc))
+             (loop (cdr as) (cdr bs) (cons (list (car as) (car bs)) acc))))))
 
 ;; not really a good blend, but ah well
 (define (blend-colors a b)
-    (+ (/ a 2) (/ b 2)))
+  (+ (/ a 2) (/ b 2)))
 
 (define (extend-rainbow!)
-    (set 'rainbow-colors
-         (mappend (lambda (colors)
-                    (list (first colors)
-                          (blend-colors (first colors) (second colors))
-                          (second colors)))
-                  (list-to-pairs rainbow-colors))))
+  (set 'rainbow-colors
+       (mappend (lambda (colors)
+                  (list (first colors)
+                        (blend-colors (first colors) (second colors))
+                        (second colors)))
+                (list-to-pairs rainbow-colors))))
 
 (extend-rainbow!)
 (extend-rainbow!)
@@ -95,78 +95,73 @@
 
 (define colors rainbow-colors)
 
-;; XXX FIXME the above does not work with optimizations enabled
-(set '#/lang/*note-closed-over-vars* #t)
-(set '#/lang/*enable-inline-let-bound-lambdas* #t)
-(set '#/lang/*enable-inline-letrec-bound-lambdas* #t)
-
 (define (move-box box)
-    (let* ((dx (aget box 2))
-           (dy (aget box 3))
-           (nx (+i (aget box 0) dx))
-           (ny (+i (aget box 1) dy))
-           (sw (point-x screen-size))
-           (sh (point-y screen-size))
-           (mx (point-x mouse-position))
-           (my (point-y mouse-position))
-           (nc (cdr (aget box 4)))
-           (sc (+i 1 (aget box 5))))
-      (when (<i nx 0)
-        (set! nx 0)
-        (set! dx (*i -1 dx)))
-      (when (<i ny 0)
-        (set! ny 0)
-        (set! dy (*i -1 dy)))
-      (when (>i nx sw)
-        (set! nx sw)
-        (set! dx (*i -1 dx)))
-      (when (>i ny sh)
-        (set! ny sh)
-        (set! dy (*i -1 dy)))
-      (when (nil? nc)
-        (set! nc colors))
-      (when (eq 0 (%i sc 233)) (set! dx (*i -1 dx)))
-      (when (eq 0 (%i sc 172)) (set! dy (*i -1 dy)))
-      (when (eq 0 (%i sc 389)) (let ((tmp dy))
-                                 (set! dy dx)
-                                 (set! dx tmp)))
-      (aset box 0 nx)
-      (aset box 1 ny)
-      (aset box 2 dx)
-      (aset box 3 dy)
-      (aset box 4 nc)
-      (aset box 5 sc)))
+  (let* ((dx (aget box 2))
+         (dy (aget box 3))
+         (nx (+i (aget box 0) dx))
+         (ny (+i (aget box 1) dy))
+         (sw (point-x screen-size))
+         (sh (point-y screen-size))
+         (mx (point-x mouse-position))
+         (my (point-y mouse-position))
+         (nc (cdr (aget box 4)))
+         (sc (+i 1 (aget box 5))))
+    (when (<i nx 0)
+      (set! nx 0)
+      (set! dx (*i -1 dx)))
+    (when (<i ny 0)
+      (set! ny 0)
+      (set! dy (*i -1 dy)))
+    (when (>i nx sw)
+      (set! nx sw)
+      (set! dx (*i -1 dx)))
+    (when (>i ny sh)
+      (set! ny sh)
+      (set! dy (*i -1 dy)))
+    (when (nil? nc)
+      (set! nc colors))
+    (when (eq 0 (%i sc 233)) (set! dx (*i -1 dx)))
+    (when (eq 0 (%i sc 172)) (set! dy (*i -1 dy)))
+    (when (eq 0 (%i sc 389)) (let ((tmp dy))
+                               (set! dy dx)
+                               (set! dx tmp)))
+    (aset box 0 nx)
+    (aset box 1 ny)
+    (aset box 2 dx)
+    (aset box 3 dy)
+    (aset box 4 nc)
+    (aset box 5 sc)))
 
 (define (add-box p &opt (dx 1) (dy 1) (priority 0))
-    (let ((box (make-array 6)))
-      (aset box 0 (point-x p))
-      (aset box 1 (point-y p))
-      (aset box 2 dx)
-      (aset box 3 dy)
-      (aset box 4 colors)
-      (aset box 5 0)
-      (sync add-box-lock (set 'boxes (cons box boxes)))
-      (fork-with-priority priority
-                          (let ((box box))
-                            (forever
-                             (sleep-ms 16)
-                             (move-box box))))))
+  (let ((box (make-array 6)))
+    (aset box 0 (point-x p))
+    (aset box 1 (point-y p))
+    (aset box 2 dx)
+    (aset box 3 dy)
+    (aset box 4 colors)
+    (aset box 5 0)
+    (sync add-box-lock (set 'boxes (cons box boxes)))
+    (fork-with-priority priority
+                        (let ((box box))
+                          (forever
+                           (sleep-ms 16)
+                           (move-box box))))))
 
 (define (clear-screen)
-    (fill-rect back-buffer 0@0 screen-size 0xffffffff))
+  (fill-rect back-buffer 0@0 screen-size 0xffffffff))
 
 (define (flip-buffer)
-    (blit-to-screen back-buffer 0@0 100 0))
+  (blit-to-screen back-buffer 0@0 100 0))
 
 (define (draw-one-box box)
-    (let* ((x (aget box 0))
-           (y (aget box 1))
-           (sc (/i (aget box 5) 275))
-           (d (if (<i sc 2) 2 (if (<i sc 6) sc 6)))
-           (color (car (aget box 4)))
-           (a (make-point x y))
-           (b (point+ a (make-point d d))))
-      (fill-rect back-buffer a b color)))
+  (let* ((x (aget box 0))
+         (y (aget box 1))
+         (sc (/i (aget box 5) 275))
+         (d (if (<i sc 2) 2 (if (<i sc 6) sc 6)))
+         (color (car (aget box 4)))
+         (a (make-point x y))
+         (b (point+ a (make-point d d))))
+    (fill-rect back-buffer a b color)))
 
 (define stack-max 0)
 (define (check-stack)
@@ -191,7 +186,7 @@
 
 (define (onmousedown p) (add-box p))
 (define (add-some-boxes p)
-    (add-box p)
+  (add-box p)
   (add-box (point+ p 0@20) -1 2)
   (add-box (point+ p 0@10) 1 -2)
   (add-box (point+ p -20@10) 3 1)
@@ -213,22 +208,22 @@
 (fork-with-priority 100000 (draw-curr-boxes))
 
 (define (make-source priority p &opt (dx 1) (dy 1))
-    (let ((box (make-array 6)))
-      (aset box 0 (point-x p))
-      (aset box 1 (point-y p))
-      (aset box 2 dx)
-      (aset box 3 dy)
-      (aset box 4 colors)
-      (aset box 5 0)
-      (fork-with-priority
-       priority
-       (forever (move-box box)
-                (add-some-boxes (make-point (aget box 0) (aget box 1)))
-                (sleep-ms 200)))))
+  (let ((box (make-array 6)))
+    (aset box 0 (point-x p))
+    (aset box 1 (point-y p))
+    (aset box 2 dx)
+    (aset box 3 dy)
+    (aset box 4 colors)
+    (aset box 5 0)
+    (fork-with-priority
+     priority
+     (forever (move-box box)
+              (add-some-boxes (make-point (aget box 0) (aget box 1)))
+              (sleep-ms 200)))))
 
 (when #f
   (define (onshow)
-      (make-source 50 (make-point (/ screen-width 2) (/ screen-height 5)) 4 5)
+    (make-source 50 (make-point (/ screen-width 2) (/ screen-height 5)) 4 5)
     (make-source 50 (make-point (/ screen-width 3) (/ screen-height 2)) 2 -1)
     (make-source 50 (make-point (/ screen-width 4) (/ screen-height 2)))
     (make-source 50 (make-point (/ screen-width 6) (/ screen-height 3)) 1 -3)
@@ -241,8 +236,8 @@
 
 
 (define (onkey k)
-    (print "----------------------------------------")
-    (#/lang/%print-stats)
+  (print "----------------------------------------")
+  (#/lang/%print-stats)
   (#/lang/%clear-stats)
   (print "cleared stats"))
 

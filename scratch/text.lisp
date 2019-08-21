195938396c96f68d62a231f5bdd60c7fbcdace27
Jason Aeschliman <j.aeschliman@gmail.com>
2019-08-21 06:15:08 -0400

    basic text-drawing utility (very basic)

---
+++ scratch/text.lisp
@@ -0,0 +1,39 @@
+(define font (load-image "./res/charmap-futuristic_white.png"))
+(define font-start 32)
+(define font-chars-per-row 18)
+(define font-char-width 7)
+(define font-char-height 9)
+(define font-char-size (make-point font-char-width font-char-height))
+
+(define (blit-charcode-at output raw-code point scale rotation)
+  (let* ((code (- raw-code font-start))
+         (col (% code font-chars-per-row))
+         (row (/ code font-chars-per-row))
+         (origin (make-point (* col font-char-width)
+                             (* row font-char-height))))
+    (blit
+     font output point
+     origin (point+ origin font-char-size)
+     scale rotation)))
+
+(defmacro %do-char-codes (binds & body)
+  (let ((ch (gensym))
+        (code (car binds))
+        (idx (cadr binds))
+        (str (caddr binds)))
+    `(let ((,idx 0))
+       (string-do-chars (,ch ,str)
+                        (let ((,code (char-code ,ch))) ,@body)
+                        (set! ,idx (+ ,idx 1))))))
+
+(define (%display-string output at scale rotation str)
+  (let ((w (* font-char-width scale)))
+    (%do-char-codes (code idx str)
+       (let ((left (f->i (* idx w))))
+         (blit-charcode-at
+          output code (point+ at (make-point left 0)) scale rotation)))))
+
+
+(define (draw-string output str at-point height rotation)
+  (let ((scale (/ height (i->f font-char-height))))
+    (%display-string output at-point scale rotation str)))

533e82207609269e6b42a5b8c2e5ef02ca1527bb
Jason Aeschliman <j.aeschliman@gmail.com>
2019-08-24 10:15:59 -0400

    sws: accept clicks

--- scratch/sws.lisp
+++ scratch/sws.lisp
@@ -2,13 +2,15 @@
 
 
 (defparameter *translation* 0@0)
+(defparameter *widget* '())
 (defparameter *buffer* '())
 
 (define (wget w k) (ht-at w k))
 (define (wset w k v) (ht-at-put w k v))
 
 (defmacro with-widget (binds & body)
-  `(binding ((*translation* (point+ *translation* (wget ,(car binds) :pos))))
+  `(binding ((*translation* (point+ *translation* (wget ,(car binds) :pos)))
+             (*widget* ,(car binds)))
      ,@body))
 
 (define (paint-rect origin size color)
@@ -37,14 +39,27 @@
     (draw-widget root))
   (blit-to-screen (wget root :buffer) 0@0 100 0))
 
-
 (define (%rect-draw w)
   (paint-rect 0@0 (wget w :size) (wget w :color)))
 
-(define (make-rect x y w h color)
+(define (make-rect x y w h color &opt (click '()))
   (let ((r (make-ht)))
     (wset r :pos (make-point x y))
     (wset r :size (make-point w h))
     (wset r :color color)
     (wset r :draw %rect-draw)
+    (wset r :click click)
     r))
+
+(define (point-in-bounds? p)
+  (not (or (<i (point-x p) (point-x *translation*))
+           (<i (point-y p) (point-y *translation*))
+           (>i (point-x p) (point-x (point+ (wget *widget* :size) *translation*)))
+           (>i (point-x p) (point-x (point+ (wget *widget* :size) *translation*))))))
+
+(define (accept-click w p)
+  (with-widget (w)
+    (when (point-in-bounds? p)
+      (let ((fn (wget w :click)))
+        (unless (nil? fn) (fn w p)))
+      (dolist (k (wget w :kids)) (accept-click k p)))))

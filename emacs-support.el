f11380bb34feee37a80da7059c66994510aad075
Jason Aeschliman <j.aeschliman@gmail.com>
2019-09-09 09:12:27 -0400

    more emacs indent support

--- emacs-support.el
+++ emacs-support.el
@@ -34,3 +34,4 @@
 (put 'binding 'common-lisp-indent-function (get 'let* 'common-lisp-indent-function))
 (put 'define 'common-lisp-indent-function (get 'let* 'common-lisp-indent-function))
 (put '%let 'common-lisp-indent-function (get 'let* 'common-lisp-indent-function))
+(put 'string-do-chars 'common-lisp-indent-function (get 'dolist 'common-lisp-indent-function))

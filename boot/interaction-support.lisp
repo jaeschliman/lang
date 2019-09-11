8f21d6066edd83f22d29139edb217aa2ad907641
Jason Aeschliman <j.aeschliman@gmail.com>
2019-09-11 09:14:42 -0400

    more indentation fixes

--- boot/interaction-support.lisp
+++ boot/interaction-support.lisp
@@ -18,18 +18,18 @@
 (at-boot (define pending-events '()))
 
 (define (handle-event e)
-    (let ((name (car e))
-          (data (cdr e)))
-      (case name
-        (onmousemove (onmousemove data))
-        (onmousedown (onmousedown data))
-        (onmouseup   (onmouseup   data))
-        (onmousedrag (onmousedrag data))
-        (onkey       (onkey       data))
-        (onshow      (onshow      data)))))
+  (let ((name (car e))
+        (data (cdr e)))
+    (case name
+      (onmousemove (onmousemove data))
+      (onmousedown (onmousedown data))
+      (onmouseup   (onmouseup   data))
+      (onmousedrag (onmousedrag data))
+      (onkey       (onkey       data))
+      (onshow      (onshow      data)))))
 
 (define (poll-for-pending-events)
-    (semaphore-wait event-ready-semaphore)
+  (semaphore-wait event-ready-semaphore)
   (let ((found pending-events)) ;; clearly not thread safe ; )
     (unless (nil? found)
       (set 'pending-events '())
@@ -39,16 +39,16 @@
 (at-boot
  (let ((started-event-loop #f))
    (define (start-event-loop)
-       (unless started-event-loop
-         (fork-with-priority 30000 (forever (poll-for-pending-events))))
+     (unless started-event-loop
+       (fork-with-priority 30000 (forever (poll-for-pending-events))))
      (set! started-event-loop #t))))
 
 ;; TODO this should no longer be needed
 (define (start-additional-event-loop)
-    (fork-with-priority 20000 (forever (poll-for-pending-events))))
+  (fork-with-priority 20000 (forever (poll-for-pending-events))))
 
 (define (request-display w h)
-    (set 'wants-display (make-point w h))
+  (set 'wants-display (make-point w h))
   (start-event-loop))
 
 'done

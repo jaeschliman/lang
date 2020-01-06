385793419094ac160eda86c0aaffcf2ad214aa44
Jason Aeschliman <j.aeschliman@gmail.com>
2020-01-06 11:19:55 -0500

    more read tests

--- scratch/xvec-read-test.lisp
+++ scratch/xvec-read-test.lisp
@@ -4,5 +4,14 @@
 (define *read-stream (xvec/make-meta-xvec-input *x))
 
 (print-object '(+ 2 2) *x)
+(print (parse-input 'meta *read-stream))
 
+(xvec/xvec-reset! *x)
+
+(print-object '(+ 3 4) *x)
+(print (parse-input 'meta *read-stream))
+
+(xvec/xvec-reset! *x)
+
+(print-object '(lambda (x) x) *x)
 (print (parse-input 'meta *read-stream))

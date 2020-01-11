933561d220a0ce7bbe35348b71845901b6933c40
Jason Aeschliman <j.aeschliman@gmail.com>
2020-01-11 04:58:57 -0800

    [WIP] special #/st package for chitchat, move grammar
    not really happy with what this does to the interop, but it's a start

---
+++ scratch/chit-chat.grammar.lisp
@@ -0,0 +1,39 @@
+;; -*- mode: fundamental -*-
+
+meta chitchat {
+  escaped-char = "\\" any:x -> x
+  comment      = "\"" (~[\"\\] any | escaped-char)* "\"" 
+  string       = "'" (~['\\] any | escaped-char)*:chs "'" -> (charlist-to-string chs)
+  ws           = (space | comment)*
+  binop-identx = "+" -> '+ | "-" -> '- | "/" -> '/ | "*" -> '*
+  captured-id  = [a-zA-Z]:fst [a-zA-Z0-9]*:rst -> (implode (cons fst rst))
+  binop-ident  = ("+"|"-"|"*"|"/"):x -> (as-message-send (list x))
+  unary-ident  = [a-zA-Z]:fst [a-zA-Z0-9]*:rst -> (as-message-send (cons fst rst))
+  nary-part    = [a-zA-Z]:fst [a-zA-Z0-9]*:rst ":" -> (append (cons fst rst) '(#\:))
+  keyword      = "self" -> 'self | "true" -> #t | "false" -> #f
+  unary-send   = rcvr:r ws unary-ident:msg ~":" -> `(send ',msg ,r)
+  binary-send  = rcvr:r ws binop-ident:msg ws expr:arg -> `(send ',msg ,r ,arg)
+  nary-send    = rcvr:r (ws nary-part:m ws subexpr:a -> (cons m a))+:msg -> (compose-msg-send r msg)
+  rcvr         = atom | group
+  local        = unary-ident:x -> `(load ,x)
+  lisp         = "`" lisp.expr:x -> `(lisp ,x)
+  atom         = keyword | lisp | local | lisp.integer | lisp.float | string | block
+  group        = "(" expr:x ")" -> x
+  arglist      = (ws ":" unary-ident)*:vs ws "|" -> vs
+  block        = "[" arglist?:args body:b "]" -> `(block :args ,args ,@b)
+  subexpr      = unary-send | binary-send | atom | group 
+  expr         = ws (nary-send | subexpr):x ws -> x
+  assign       = unary-ident:var ws ":=" expr:val -> `(set! ,var ,val)
+  return       = "^" ws expr:x -> `(return ,x)
+  stmt         = return | assign | expr
+  stmts        = ws stmt:s (ws "." ws stmt)*:ss -> (cons s ss)
+  vars         = "|" (ws unary-ident)*:vars ws "|" -> vars
+  body         = ws vars?:vars stmts:stmts -> `(:vars ,vars :body ,stmts) 
+
+  nary-hdr     = (nary-part:m ws unary-ident:a ws -> (cons m a))+:hdr -> (compose-hdr hdr)
+  unary-hdr    = unary-ident:m -> (list :name m :args '())
+  binary-hdr   = binop-ident:m ws unary-ident:arg -> (list :name m :args (list arg))
+  method-hdr   = nary-hdr | unary-hdr  | binary-hdr
+  method-defn  = captured-id:cls ">>" method-hdr:h ws "[" body:b "]" -> `(method :class ,cls ,@h ,@b)
+  file-in      = (ws method-defn)+:ms ws -> `(chitchat-methods ,ms)
+}
--- scratch/chit-chat.lisp
+++ scratch/chit-chat.lisp
@@ -1,14 +1,24 @@
 (set '*package* (symbol-package 'define))
 
+(define %message-package (%make-package "messages"))
+(package-add-subpackage %root-package %message-package "st")
+
+(define (as-message-send charlist)
+  (let ((r (intern (charlist-to-string charlist) %message-package)))
+    (print r)
+    r))
+
+(define (rulename rule) (intern (symbol-name rule) %meta-package))
+
 (define (compose-msg-send rcvr parts)
-  (let ((msg (implode (reduce-list (lambda (acc pair)
-                                     (append acc (car pair)))
-                                   '() parts)))
+  (let ((msg (as-message-send (reduce-list (lambda (acc pair)
+                                             (append acc (car pair)))
+                                           '() parts)))
         (args (mapcar cdr parts)))
     `(send ',msg ,rcvr ,@args)))
 
 (define (compose-hdr parts)
-  (let ((msg (implode (reduce-list (lambda (acc pair)
+  (let ((msg (as-message-send (reduce-list (lambda (acc pair)
                                      (append acc (car pair)))
                                    '() parts)))
         (args (mapcar cdr parts)))
@@ -16,46 +26,9 @@
 
 (print 'begin)
 
-meta chitchat {
-  escaped-char = "\\" any:x -> x
-  comment      = "\"" (~[\"\\] any | escaped-char)* "\"" 
-  string       = "'" (~['\\] any | escaped-char)*:chs "'" -> (charlist-to-string chs)
-  ws           = (space | comment)*
-  binop-ident  = "+" -> '+ | "-" -> '- | "/" -> '/ | "*" -> '*
-  unary-ident  = [a-zA-Z]:fst [a-zA-Z0-9]*:rst -> (implode (cons fst rst))
-  nary-part    = [a-zA-Z]:fst [a-zA-Z0-9]*:rst ":" -> (append (cons fst rst) '(#\:))
-  keyword      = "self" -> 'self | "true" -> #t | "false" -> #f
-  unary-send   = rcvr:r ws unary-ident:msg ~":" -> `(send ',msg ,r)
-  binary-send  = rcvr:r ws binop-ident:msg ws expr:arg -> `(send ',msg ,r ,arg)
-  nary-send    = rcvr:r (ws nary-part:m ws subexpr:a -> (cons m a))+:msg -> (compose-msg-send r msg)
-  rcvr         = atom | group
-  local        = unary-ident:x -> `(load ,x)
-  lisp         = "`" lisp.expr:x -> `(lisp ,x)
-  atom         = keyword | lisp | local | lisp.integer | lisp.float | string | block
-  group        = "(" expr:x ")" -> x
-  arglist      = (ws ":" unary-ident)*:vs ws "|" -> vs
-  block        = "[" arglist?:args body:b "]" -> `(block :args ,args ,@b)
-  subexpr      = unary-send | binary-send | atom | group 
-  expr         = ws (nary-send | subexpr):x ws -> x
-  assign       = unary-ident:var ws ":=" expr:val -> `(set! ,var ,val)
-  return       = "^" ws expr:x -> `(return ,x)
-  stmt         = return | assign | expr
-  stmts        = ws stmt:s (ws "." ws stmt)*:ss -> (cons s ss)
-  vars         = "|" (ws unary-ident)*:vars ws "|" -> vars
-  body         = ws vars?:vars stmts:stmts -> `(:vars ,vars :body ,stmts) 
-
-  nary-hdr     = (nary-part:m ws unary-ident:a ws -> (cons m a))+:hdr -> (compose-hdr hdr)
-  unary-hdr    = unary-ident:m -> (list :name m :args '())
-  binary-hdr   = binop-ident:m ws unary-ident:arg -> (list :name m :args (list arg))
-  method-hdr   = nary-hdr | unary-hdr  | binary-hdr
-  method-defn  = unary-ident:cls ">>" method-hdr:h ws "[" body:b "]" -> `(method :class ,cls ,@h ,@b)
-  file-in      = (ws method-defn)+:ms ws -> `(chitchat-methods ,ms)
-}
-;;"
+(import-file! "./scratch/chit-chat.grammar.lisp")
 
 (print 'done-meta)
-
-(define (rulename rule) (intern (symbol-name rule) %meta-package))
 
 (define (dbg rule str)
   (stream-write-string *standard-output* str)
@@ -69,8 +42,11 @@ meta chitchat {
 (define (dbgx rule str)
   (stream-write-string *standard-output* str)
   (newline)
-  (binding ((*meta-context* '(chitchat)))
-    (match-map (lambda (e) (print (compiler e))) (rulename rule) str))
+  (try-catch (lambda ()
+                (binding ((*meta-context* '(chitchat)))
+                         (match-map (lambda (e) (print (compiler e))) (rulename rule) str)))
+             (lambda (ex) (print `(whoops! ,x))))
+
   (stream-write-string *standard-output* "================================\n"))
 
 (define (dbge rule str)
@@ -110,7 +86,8 @@ meta chitchat {
      ^ [ :x | x + y ]
 ")
 
-(%load "./scratch/chit-chat.compiler.lisp")
+
+(import-file! "./scratch/chit-chat.compiler.lisp")
 
 (dbgx 'file-in "
 Array>>first [ ^ self at: 0 ]
@@ -119,16 +96,16 @@ LazyTable>>at:x put:y [
   storage ifNil: [ storage := HashTable new ].
   ^ storage at: x put:y
 ]
-Cons>>collect: block [ ^ `(mapcar (lambda (it) (@send 'value: block it)) self) ]
+Cons>>collect: block [ ^ `(mapcar (lambda (it) (@send '#/st/value: #/st/block it)) self) ]
 Fixnum>>pi [ ^ `*pi* ]
 ")
 
 
 (dbge 'file-in "
-Fixnum>>+ other [ ^ `(+ self other) ]
+Fixnum>>+ other [ ^ `(+ self #/st/other) ]
 Closure>>value [ ^ `(self) ]
 Boolean>>ifTrue: then ifFalse: else [
- ^ `(if self (@send 'value then) (@send 'value else))
+ ^ `(if self (@send '#/st/value #/st/then) (@send '#/st/value #/st/else))
 ]
 Fixnum>>isEven [ ^ `(= 0 (% self 2)) ]
 Fixnum>>aCheck [
@@ -143,19 +120,18 @@ Fixnum>>anotherCheck [
 ]
 ")
 
-
-(print (@send '+ 2 3))
-(print (@send 'value (lambda () 10)))
-(print (@send 'isEven 4))
-(print 'ifTrue:ifFalse:)
-(let ((even? (@send 'isEven 4)))
+(print (@send '#/st/+ 2 3))
+(print (@send '#/st/value (lambda () 10)))
+(print (@send '#/st/isEven 4))
+(print '#/st/ifTrue:ifFalse:)
+(let ((even? (@send '#/st/isEven 4)))
   (print `(even? ,even?))
-  (print (@send 'ifTrue:ifFalse: even? (lambda () "ok") (lambda () "not ok"))))
-(print (@send 'ifTrue:ifFalse: (@send 'isEven 4) (lambda () "ok") (lambda () "not ok")))
-(print (@send 'aCheck 4))
+  (print (@send '#/st/ifTrue:ifFalse: even? (lambda () "ok") (lambda () "not ok"))))
+(print (@send '#/st/ifTrue:ifFalse: (@send '#/st/isEven 4) (lambda () "ok") (lambda () "not ok")))
+(print (@send '#/st/aCheck 4))
 (print `(expecting early exit))
-(print (@send 'anotherCheck 4))
+(print (@send '#/st/anotherCheck 4))
 (print `(expecting was not even))
-(print (@send 'anotherCheck 3))
+(print (@send '#/st/anotherCheck 3))
 
 (print 'done)
